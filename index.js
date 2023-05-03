29649db7e548c559681d75965f7fdc1170983645
istiaq-tanim <istiaq.tanim17@gmail.com>
2023-05-03 09:35:06 +0600

    chef data added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
+
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 const chefs=require("./data/chefs.json")
@@ -8,8 +11,15 @@ app.get("/",(req,res)=> {
     res.send("Chef is Running")
 })
 
+
 app.get("/chefs",(req,res)=> {
     res.send(chefs);
+})
+
+app.get("/chefs/:id",(req,res)=>{
+    const chefId=parseInt(req.params.id);
+    const chefData=chefs.find(item => parseInt(item.id) === chefId);
+    res.send(chefData);
 })
 
 app.listen(port,()=>{
